8bf9a2f267692184214fe2ca90630509064d28a2
spartacusrex <patrickcerri@gmail.com>
2024-02-21 15:17:55 +0000

    erc20 balance and send

--- dapp/js/ethutil.js
+++ dapp/js/ethutil.js
@@ -15,6 +15,52 @@ var wMinimaInterfaceABI = new ethers.utils.Interface(WMINIMA_ABI.abi);
 var wMinimaContractAddress = "0x95401dc811bb5740090279ba06cfa8fcf6113778";
 
 /**
+ * the Main ETH Wallet used 
+ */
+var MAIN_WALLET = null;
+
+/**
+ * Keep track of the nonce..
+ */
+var NONCE_TRACK = 0;
+
+/**
+ * Initialise the ETH subsystem
+ */
+function initialiseETH(privatekey, callback){
+	
+	//Create a wallet..
+	MAIN_WALLET = new ethers.Wallet(privateKey);
+	
+	//And now set up the nonce..
+	setNonceAuto(MAIN_WALLET.address,function(){
+		MDS.log("ETH Wallet setup : "+MAIN_WALLET.address+" nonce:"+NONCE_TRACK);
+		
+		if(callback){
+			callback();
+		}
+	});
+}
+
+/**
+ * Auto-set the NONCE
+ */
+function setNonceAuto(address,callback){
+	
+	//Get the nonce..
+	getTransactionCount(address,function(txncount){
+				
+		//This is the nonce..
+		NONCE_TRACK = parseInt(txncount,16);
+		
+		//And send back..
+		if(callback){
+			callback(NONCE_TRACK);	
+		}
+	});
+}
+
+/**
  * Run an ETH command  
  */
 function runEthCommand(payload, callback){
@@ -68,6 +114,14 @@ function getETHWeiBalance(address, callback) {
 	});
 }
 
+function getETHBalance(address, callback) {
+	getETHWeiBalance(address,function(ethresp){
+		//Convert to ETH
+		var ethvalue = ethers.utils.formatEther(ethresp);
+		callback(ethvalue);
+	});
+}
+
 /**
  * Get the Nonce for an address
  */
@@ -88,15 +142,15 @@ function getRequiredNonce(address, callback) {
 		var nonce = parseInt(txncount,16);	
 		callback(nonce);
 	});
-}
+}	
 
 /**
  * Create a RAW unsigned Simple ETH Send Transaction
  */
-function createRAWSendTxn(toaddress, nonce, ethamount){
+function createRAWSendTxn(toaddress, ethamount){
 	
 	var transaction = {
-    	nonce: nonce,
+    	nonce: NONCE_TRACK,
     	gasLimit: 21000,
     	gasPrice: ethers.utils.bigNumberify("20000000000"),
     	to: toaddress,
@@ -109,10 +163,10 @@ function createRAWSendTxn(toaddress, nonce, ethamount){
 /**
  * Create a RAW unsigned Contract Call Transaction
  */
-function createRAWContractCallTxn(contractAddress, functionData, nonce){
+function createRAWContractCallTxn(contractAddress, functionData){
 	
 	var transaction = {
-    	nonce: nonce,
+    	nonce: NONCE_TRACK,
     	gasLimit: 100000,
     	gasPrice: ethers.utils.bigNumberify("20000000000"),
     	to: contractAddress,
@@ -131,6 +185,9 @@ function sendRAWSignedTxn(signedtxn,callback){
 	var payload = {"jsonrpc":"2.0", "method":"eth_sendRawTransaction",
 			"params": [signedtxn], 
 			"id": 1};
+	
+	//The NONCE MUST be incremnented..
+	NONCE_TRACK++;
 	  
 	//Run it..
 	runEthCommand(payload,function(ethresp){
@@ -141,9 +198,9 @@ function sendRAWSignedTxn(signedtxn,callback){
 /**
  * Sign and Send a RAW transaction
  */
-function postTransaction(wallet, unsignedtransaction, callback){
+function postTransaction(unsignedtransaction, callback){
 	
-	var signPromise = wallet.sign(unsignedtransaction);
+	var signPromise = MAIN_WALLET.sign(unsignedtransaction);
 	signPromise.then((signedTransaction) => {
 	    
 	    //Now send!
@@ -156,13 +213,13 @@ function postTransaction(wallet, unsignedtransaction, callback){
 /**
  * Send ETH function
  */
-function sendETH(wallet, toaddress, amount,  nonce, callback){
+function sendETH(toaddress, amount, callback){
 	
 	//Create a RAW txn
-	var txn = createRAWSendTxn(toaddress.toLowerCase(), nonce, amount+"");
+	var txn = createRAWSendTxn(toaddress.toLowerCase(), amount+"");
 	
 	//And now sign and Post It..
-	postTransaction(wallet, txn, function(ethresp){
+	postTransaction(txn, function(ethresp){
 		callback(ethresp);
 	});
 }
@@ -170,7 +227,7 @@ function sendETH(wallet, toaddress, amount,  nonce, callback){
 /**
  * Get ERC20 Balance
  */
-function getERC20Balance(address, callback){
+function getWMinimaBalance(address, callback){
 	
 	//Get ETH valid address
 	var addr = address.toLowerCase();
@@ -183,6 +240,33 @@ function getERC20Balance(address, callback){
 	
 	//Run this
 	ethCallCommand(wMinimaContractAddress,functiondata,function(ethresp){
-		callback(ethresp);	
+		var bal = ethers.utils.formatEther(ethresp.result);
+		callback(bal);	
 	});
+}
+
+/**
+ * Send wMinima ERC20
+ */
+function sendWMinima(toaddress, amount, callback){
+	
+	//Get ETH valid address
+	var addr = toaddress.toLowerCase();
+	if(addr.startsWith("0x")){
+		addr = addr.slice(2)
+	}
+	
+	//The actual amount - wMinima has 18 decimla places..
+	var sendamount = ethers.utils.parseUnits(""+amount,18);
+	
+	//Get the function data
+	var functiondata = wMinimaInterfaceABI.functions.transfer.encode([addr ,sendamount]);
+	
+	//Now create the RAW txn..
+	var transaction = createRAWContractCallTxn(wMinimaContractAddress, functiondata);
+	
+	//NOW SIGN..
+	postTransaction(transaction, function(ethresp){
+		callback(ethresp);
+	}); 
 }
